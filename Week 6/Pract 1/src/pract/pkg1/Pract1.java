adacede24cb23602f162d0b79a74ac8f5d3f03c3
swatikanwal <kanwalswati97@gmail.com>
2017-12-30 01:46:15 +0530

    Threading

---
+++ Week 6/Pract 1/src/pract/pkg1/Pract1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pract.pkg1;
+
+/**
+ *
+ * @author swatiii
+ */
+class threads implements Runnable{
+    threads(int n){
+        Thread temp = new Thread(this,String.valueOf(n));
+        System.out.println("Thread number "+n+ " created");
+        temp.start();
+    }
+    public void run(){
+        
+    }
+}
+public class Pract1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
